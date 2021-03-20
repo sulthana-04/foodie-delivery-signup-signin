f19494cc1c226871fc62f876080234d96c587a44
sulthana-parveen <76679665+sulthana-parveen@users.noreply.github.com>
2021-03-20 13:14:43 -0700

    code

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app =  express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+require('dotenv/config');
+
+const authRoute = require('./routes/auth');
+
+app.use(bodyParser.json());
+app.use(express.json());
+app.use('/api/user', authRoute);
+ 
+//connect to db
+const mongoUri="mongodb+srv://sulthana:sulthana@crud.xngbi.mongodb.net/sulthana?retryWrites=true&w=majority"
+mongoose.connect(
+ mongoUri,
+{useNewUrlParser: true },
+() => console.log('connect to DB!')
+);
+app.listen(5000, () => console.log('server up and running'));
